06ae7ee67025c6a1229d799f0d87b53bd5d499b2
Brendon Ngirazi <48953676+Ju99ernaut@users.noreply.github.com>
2024-07-02 08:50:19 +0200

    add font

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,11 +1,29 @@
 import type { Plugin } from 'grapesjs'
 
-export type PluginOptions = {}
+export type PluginOptions = {
+    section?: string;
+    property?: string;
+}
 
 export const plugin: Plugin<PluginOptions> = (editor, opts = {}) => {
   const options: PluginOptions = {
+    section: 'typography',
+    property: 'font-family',
     ...opts,
   }
+
+  const { section, property } = options
+
+  editor.Commands.add('add-google-font', {
+    run(editor, _sender, options) {
+      const { id, label } = options
+      const font = { id, label: label ?? id }
+      const prop = editor.StyleManager.getProperty(String(section), String(property))
+      // @ts-ignore
+      prop?.setOptions([font, ...prop.getOptions()])
+    },
+    stop() {},
+  })
 }
 
 export default plugin
